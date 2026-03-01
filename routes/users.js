bee5be596383df72418134e79ca5026ea827271f
FlaTy7375 <flaty7375@gmail.com>
2026-03-01 18:30:17 +0300

    feat: created registration and login routes for the user

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-import { login, register, current } from '../controllers/users';
+const { login, register, current } = require('../controllers/users');
 
 // api/user/login
-router.post('/login', login());
+router.post('/login', login);
 
 // api/user/register
-router.post('/register', register());
+router.post('/register', register);
 
 // api/user/current
-router.get('/current', current());
+router.get('/current', current);
 
 module.exports = router;
