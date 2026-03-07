caf06dc192cd173e36f448bfd950fd03b5b6dc25
FlaTy7375 <flaty7375@gmail.com>
2026-03-07 15:26:47 +0300

    Feat: The registration page has been implemented

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,9 +1,28 @@
+import { Card, Form, Row, Space, Typography } from "antd"
+import { Link } from "react-router-dom"
 import { Layout } from "../../components/layout/index.tsx"
+import { CustomInput } from "../../components/custom-input/index.tsx"
+import { PasswordInput } from "../../components/password-input/index.tsx"
+import { CustomButton } from "../../components/custom-button/index.tsx"
+import { Paths } from "../../paths.ts"
 
 export const Register = () => {
   return (
     <Layout>
-        <div>Register</div>
+        <Row align={"middle"} justify={"center"}>
+            <Card title={"Создайте свой аккаунт!"} style={{width: "30rem"}}>
+                <Form onFinish={() => null}>
+                    <CustomInput type="text" name="name" placeholder="Имя"></CustomInput>
+                    <CustomInput type="email" name="email" placeholder="Email"></CustomInput>
+                    <PasswordInput name="password" placeholder="Пароль"></PasswordInput>
+                    <PasswordInput name="confirm" placeholder="Повторите пароль"></PasswordInput>
+                    <CustomButton type="primary" htmlType="submit">Зарегистрироваться</CustomButton>
+                </Form>
+                <Space dir="vertical" size="large">
+                    <Typography.Text>Уже есть аккаунт? <Link to={ Paths.login }>Войти</Link></Typography.Text>
+                </Space>
+            </Card>
+        </Row>
     </Layout>
   )
 }
